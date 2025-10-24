c3005c80496e114f404c6fb9289a64a6eeae5f2d
Arezim Legion <arezimt@gmail.com>
2025-10-24 23:51:45 +0200

    motion-planning 2

---
+++ motion-planning-2/build/my_robot_description/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/tom/ws_urdf/src/my_robot_description
+# Build directory: /home/tom/ws_urdf/build/my_robot_description
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
