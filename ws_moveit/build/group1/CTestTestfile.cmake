f515be7fedd11b7ff8bea0df3a092f9258bae5f9
Tristanko <tristan.dormans@gmail.com>
2026-01-13 13:14:55 +0100

    Add: /ws_moveit/build to .gitignore

--- ws_moveit/build/group1/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /root/test/ws_moveit/src/group1
-# Build directory: /root/test/ws_moveit/build/group1
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
