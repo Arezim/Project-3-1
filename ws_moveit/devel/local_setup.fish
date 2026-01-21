3f36a205b22056c2b0b3a082171b43ea81121056
Tristan <62807768+Tristan-Dormans@users.noreply.github.com>
2026-01-21 21:40:06 +0100

    Update .gitignore

--- ws_moveit/devel/local_setup.fish
+++ ws_moveit/devel/local_setup.fish
@@ -1,14 +1,1 @@
-#!/usr/bin/env fish
-# generated from catkin/cmake/template/local_setup.fish.in
-
-# since this file is sourced either use the provided _CATKIN_SETUP_DIR
-# or fall back to the destination set at configure time
-
-if test -z $_CATKIN_SETUP_DIR
-    set _CATKIN_SETUP_DIR /home/nishan/Project-3-1/ws_moveit/devel
-end
-
-set CATKIN_SETUP_UTIL_ARGS "--extend --local"
-source "$_CATKIN_SETUP_DIR/setup.fish"
-
-set -e CATKIN_SETUP_UTIL_ARGS
+/root/test/Project-3-1/ws_moveit/devel/.private/catkin_tools_prebuild/local_setup.fish
